0858e7cfd44fd4fd550b99553928edcb4869db62
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-08-11 13:33:21 +0545

    made some changes in filing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "../src/Components/Navbar/Navbar";
 export  class App extends Component {
   render() {
     return (
       <div>
-        <h1>
-       please install tailwind css routing 
-        </h1>
-        
-      </div>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
     )
   }
 }
